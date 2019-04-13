3d11f4aa36e6cc42be1e419fa5dc8b428eddfd14
Guillermo Constantino <guilleconstantino@gmail.com>
2019-04-13 22:07:05 +0200

    Allow creation of videos from API

--- api/rest/viewsets.py
+++ api/rest/viewsets.py
@@ -3,22 +3,21 @@
 '''
 
 from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
 from api.rest.serializers.category_seriazer import CategorySerializer
 from api.rest.serializers.video_seriazer import VideoSerializer
-from api.rest.permissions import AllowAnyOnCreation
 from api import models
-
  
 
-class CategoryViewSet(AllowAnyOnCreation, viewsets.ReadOnlyModelViewSet):
+class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
     """ A view set that allows to display all the available categories """
     queryset = models.Category.objects.filter(enabled=True)
     serializer_class = CategorySerializer
 
 
-class VideoViewSet(AllowAnyOnCreation, viewsets.ReadOnlyModelViewSet):
+class VideoViewSet(viewsets.ModelViewSet):
     """ A view set that allows to display all the videos """
     queryset = models.Video.objects.all()
     serializer_class = VideoSerializer
-
+    permission_classes = (AllowAny, )
 
--- videos/settings.py
+++ videos/settings.py
@@ -139,7 +139,7 @@ MEDIA_URL = '/media/'
 # Rest
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
+      
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
